e3fb4c8af0c621ca43935a1cb595b13fa876e78c
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2023-06-06 07:51:13 +0800

    added partytown and google analytics 4 script tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [
+    vue(),
+    tailwind(),
+    partytown({
+      config: { 
+        forward: ["dataLayer.push"] 
+      },
+    })]
 });
