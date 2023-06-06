dee53d56befcc74962e690ee489f7ff44bbafa6f
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2023-06-06 08:30:30 +0800

    added astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(),
-    tailwind(),
-    partytown({
-      config: { 
-        forward: ["dataLayer.push"] 
-      },
-    })]
+  site: 'https://jezreldave.com',
+  integrations: [vue(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()]
 });
